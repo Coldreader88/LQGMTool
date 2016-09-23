e9f0f2d01dd01210d3248c8bfc14200e22cca2a1
keyongyu <247321453@qq.com>
2016-09-23 17:29:13 +0800

    tool

---
+++ GMTool/TextConvert/Converter.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： Administrator
+ * 日期: 2016/9/23
+ * 时间: 17:12
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TextConvert
+{
+	/// <summary>
+	/// Description of Converter.
+	/// </summary>
+	public class Converter
+	{
+		private string file;
+		
+		public Converter(string file)
+		{
+			this.file=file;
+		}
+		
+		public void GetList(string outfile){
+			Dictionary<string, string> lines=new Dictionary<string, string>();
+			using (FileStream fs = new FileStream(file, FileMode.Open))
+			{
+				using (StreamReader sr = new StreamReader(fs, Encoding.Unicode))
+				{
+					string line = null;
+					Regex regex = new Regex("\"(\\S*?_HEROES_\\S+?)\"\\s+\"([\\s\\S]+?)\"");
+					while ((line = sr.ReadLine()) != null)
+					{
+						Match m = regex.Match(line);
+						if(m.Groups.Count>2){
+							string name = m.Groups[1].Value;
+							string oldname = name;
+							name = GetName(name);
+							string val = oldname+"\t"+m.Groups[2].Value;
+							if(!lines.ContainsKey(name)){
+								Console.WriteLine(name+"\t"+oldname);
+								lines.Add(name, val);
+							}
+						}
+					}
+				}
+			}
+			File.Delete(outfile);
+			using (FileStream fs = new FileStream(outfile, FileMode.OpenOrCreate))
+			{
+				using (StreamWriter sw = new StreamWriter(fs, Encoding.Unicode))
+				{
+					foreach(string name in lines.Keys){
+						sw.WriteLine(lines[name]);
+					}
+				}
+			}
+		}
+		
+		private string GetName(string name){
+			int i = name.IndexOf("HEROES_");
+			if(i>=0){
+				int j = name.IndexOf("_", i+8);
+				//如果是最后一个_
+				int k = name.LastIndexOf("_");
+				if(j == k){
+					name = name.Substring(0, i+6);
+				}else  if(j>=0){
+					name = name.Substring(0, j);
+				}
+			}
+			return name;
+		}
+	}
+}
---
+++ GMTool/TextConvert/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace TextConvert
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			args = new string[]{"-l"};
+			if(args.Length > 0){
+				switch(args[0]){
+					case "-l":
+						string file = "heroes_text_taiwan.txt";
+						if(args.Length>1){
+							file = args[1];
+						}
+						string outfile="list.txt";
+						if(args.Length>2){
+							outfile = args[2];
+						}
+						new Converter(file).GetList(outfile);
+						break;
+				}
+			}
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
